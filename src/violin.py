bc1bc471cf5529789ae29cb01642c150b78210b7
truejimfrank <james.a.frank@hotmail.com>
2020-02-14 14:08:29 -0700

    iamges

--- src/violin.py
+++ src/violin.py
@@ -1,5 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from surprise import SVD, Dataset, accuracy, Reader
 from surprise.model_selection import cross_validate
+
 
 # https://surprise.readthedocs.io/en/stable/getting_started.html
 # Load the movielens-100k dataset (download it if needed),
@@ -19,12 +23,14 @@ reader = Reader(name=None,
 
 datacsv = Dataset.load_from_file('../data/movies/ratings.csv', reader=reader)
 
-cross_validate(algo, datacsv, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+# cv returns a dictionary with many result keys
+svd_cv = cross_validate(algo, datacsv, measures=['RMSE', 'MAE'], cv=5, 
+                        n_jobs=3, verbose=True)
+
+svd_cv['test_rmse'].mean() # returns mean of the error array
 
 
-
-
-# Create array of predictions for violinplot
+# Create array of predictions for violinplot (from sol notebook)
 # data = [predictions_df['prediction'][predictions_df['rating'] == rating].values for rating in range(1, 6)]
 # plt.violinplot(data, range(1,6), showmeans=True)
 # plt.xlabel('True Ratings')
