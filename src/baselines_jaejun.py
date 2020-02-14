4bda7bb472b8157348e1b276dff1354f3cbf76ba
jaejun <jaejun.lee@live.com>
2020-02-14 10:13:07 -0700

    RMSE for baseline from data folder

---
+++ src/baselines_jaejun.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+"""
+http://surprise.readthedocs.io/en/stable/building_custom_algo.html
+"""
+
+import sys
+import numpy as np
+from surprise import AlgoBase, Dataset, Reader
+from surprise.model_selection.validation import cross_validate
+import pandas as pd
+
+class GlobalMean(AlgoBase):
+    def __init__(self):
+
+        # Always call base method before doing anything.
+        AlgoBase.__init__(self)
+
+    def fit(self, trainset):
+
+        # Here again: call base method before doing anything.
+        AlgoBase.fit(self, trainset)
+
+        # Compute the average rating. We might as well use the
+        # trainset.global_mean attribute ;)
+        self.the_mean = np.mean([r for (_, _, r) in
+                                 self.trainset.all_ratings()])
+
+        return self
+
+    def estimate(self, u, i):
+
+        return self.the_mean
+
+
+class MeanofMeans(AlgoBase):
+    def __init__(self):
+
+    # Always call base method before doing anything.
+        AlgoBase.__init__(self)
+
+
+    def fit(self, trainset):
+
+        # Here again: call base method before doing anything.
+        AlgoBase.fit(self, trainset)
+
+        users = np.array([u for (u, _, _) in self.trainset.all_ratings()])
+        items = np.array([i for (_, i, _) in self.trainset.all_ratings()])
+        ratings = np.array([r for (_, _, r) in self.trainset.all_ratings()])
+
+        user_means,item_means = {},{}
+        for user in np.unique(users):
+            user_means[user] = ratings[users==user].mean()
+        for item in np.unique(items):
+            item_means[item] = ratings[items==item].mean()
+
+        self.global_mean = ratings.mean()
+        self.user_means = user_means
+        self.item_means = item_means
+
+    def estimate(self, u, i):
+        """
+        return the mean of means estimate
+        """
+
+        if u not in self.user_means:
+            return(np.mean([self.global_mean,
+                            self.item_means[i]]))
+
+        if i not in self.item_means:
+            return(np.mean([self.global_mean,
+                            self.user_means[u]]))
+
+        return(np.mean([self.global_mean,
+                        self.user_means[u],
+                        self.item_means[i]]))
+
+
+if __name__ == "__main__":
+
+    
+
+    ratings_df = pd.read_csv('../data/movies/ratings.csv',sep=',', header=0)
+
+    # A reader is still needed but only the rating_scale param is requiered.
+    reader = Reader(rating_scale=(1, 5))
+
+    # The columns must correspond to user id, item id and ratings (in that order).
+    data = Dataset.load_from_df(ratings_df[['userId', 'movieId', 'rating']], reader)
+    #data = Dataset.load_builtin('ml-100k')
+    
+    print("\nGlobal Mean...")
+    algo = GlobalMean()
+    result_grobalmean = cross_validate(algo, data, measures=['rmse'])
+    print(f"Global Mean: {np.mean(result_grobalmean['test_rmse'])}")
+
+    print("\nMeanOfMeans...")
+    algo = MeanofMeans()
+    cross_validate(algo, data)
+    result_meanofmean = cross_validate(algo, data, measures=['rmse'])
+    print(f"MeanofMeans: {np.mean(result_meanofmean['test_rmse'])}")
+
+    
